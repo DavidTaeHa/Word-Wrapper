fab8e3827b1c3a5383faffdae97162e31a1e73fd
Gandhi115 <61490541+Gandhi115@users.noreply.github.com>
2022-03-28 13:56:17 -0400

    update

--- WordWrapper/ww.c
+++ WordWrapper/ww.c
@@ -230,30 +230,25 @@ int main(int argc, char **argv)
     else if (argc == 2)
     {
         char *userStr = malloc(sizeof(char) * INPTSIZE);
-        fgets(userStr, INPTSIZE, stdin);
+
         // Creating a temporary file
         char *tempName = malloc(sizeof(char) * 11);
         char *tempNum = malloc(sizeof(char) * 6);
-        strcpy(tempName, "temp.");
+        strcpy(tempName, "temp");
         srand(time(NULL));
         int nameNum = rand() % (99999 - 10000) + 10000;
         sprintf(tempNum, "%d", nameNum);
         strcat(tempName, tempNum);
+        strcat(tempName, ".txt");
         free(tempNum);
-        
-        FILE *fp = fopen(tempName, "w+");
-        if (fp)
-        {
-            fputs(userStr, fp);
-        }
-        // If there was an error retrieving the file
-        else
-        {
-            perror(tempName);
-            exitCode = EXIT_FAILURE;
-        }
-        fclose(fp);
+
+        //Reads from stdin and prints out to a temp file
+        int outText = open(tempName, O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
+        read(0, userStr, INPTSIZE);
+        userStr[strlen(userStr)] = '\0';
+        write(outText, userStr, strlen(userStr));
         free(userStr);
+
         // If the file exists proceed
         if ((stat(tempName, &temp) != -1))
         {
@@ -265,7 +260,12 @@ int main(int argc, char **argv)
             wrap_file(inText, 1, atoi(argv[1]));
             close(inText);
         }
+        else{
+            perror(tempName);
+            exit(EXIT_FAILURE);
+        }
         // Remove temporary file as it is no longer needed and should not exist.
+        close(outText);
         remove(tempName);
         free(tempName);
     }
